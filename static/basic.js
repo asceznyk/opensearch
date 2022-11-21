210d5e2b05744f76fb77367548d472c5272320bc
asceznyk <asceznyk@gmail.com>
2022-11-21 12:33:08 +0530

    normal terminal based tqdm..

--- static/basic.js
+++ static/basic.js
@@ -1,16 +1,17 @@
 const queryHandler = document.getElementById('query');
 const resultHandler = document.getElementById('results');
 
+let results;
 queryHandler.addEventListener("keyup", async function() {
-	let results = await fetch("/", {
+	results = await fetch("/", {
 		method:"post", 
 		headers:{"content-type":"application/json"}, 
 		body: JSON.stringify({
 			"query": queryHandler.value,
 		}), 
-	});
-	console.log(results);
+	}).resolve();
 });
+console.log(results);
  
 
 
