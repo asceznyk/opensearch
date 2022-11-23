8956063ebfe982364e0f4cb4f7310418a7c197dd
asceznyk <asceznyk@gmail.com>
2022-11-23 12:09:58 +0530

    added autocomplete!

--- home.py
+++ home.py
@@ -23,9 +23,9 @@ def main_page():
         print(results)
         top_words = {"words":[], "scores":[]}
         for k, v in results.items():
-            top_words["words"].append(k.replace("'", '"'))
-            top_words["scores"].append(v)
-        return json.dumps(str(top_words))
+            top_words["words"].append(k)
+            top_words["scores"].append(f"'{v}'")
+        return jsonify(str(top_words))
     else:
         return render_template('main.html')
 
