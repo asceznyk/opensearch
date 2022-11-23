6ebbbd0f6fba5bab1635c9a0b8e8cb38102dcf42
asceznyk <asceznyk@gmail.com>
2022-11-23 20:48:58 +0530

    remove attribute..

--- home.py
+++ home.py
@@ -19,13 +19,18 @@ lsh_main = rplsh.RandomProjectionLSH(vec_space_path=f'{home_dir}/GoogleNews-vect
 @app.route("/", methods=['GET', 'POST'])
 def main_page():
     if request.method == 'POST':
-        results = lsh_main.search(request.get_json()['query'])
-        print(results)
         top_words = {"words":[], "scores":[]}
-        for k, v in results.items():
-            top_words["words"].append(f"{k}")
-            top_words["scores"].append(f"{v}")
-        return json.dumps(top_words)
+        try:
+            results = lsh_main.search(request.get_json()['query'])
+            print(results)
+            for k, v in results.items():
+                top_words["words"].append(f"{k}")
+                top_words["scores"].append(f"{v}")
+        except Exception as e:
+            print(f"error occured: {e}")
+            pass
+
+        return json.dums(top_words)
     else:
         return render_template('main.html')
 
