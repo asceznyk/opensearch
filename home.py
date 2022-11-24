44f6bea9b9d1783c7617c798c17e8ddafd54f8c4
asceznyk <asceznyk@gmail.com>
2022-11-24 12:03:16 +0530

    made it configable..

--- home.py
+++ home.py
@@ -7,7 +7,7 @@ import numpy as np
 
 from flask import Flask, render_template, request, jsonify
 
-def create_app(config):
+def main(config):
     sys.path.append(config['sys_path'])
     from rplsh import lib as rplsh
 
@@ -17,33 +17,31 @@ def create_app(config):
         load_dir=config['load_dir']
     )
 
-    return app, lsh_main
+    @app.route("/", methods=['GET', 'POST'])
+    def index():
+        if request.method == 'POST':
+            top_words = {"words":[], "scores":[]}
+            try:
+                results = lsh_main.search(request.get_json()['query'])
+                print(results)
+                for k, v in results.items():
+                    top_words["words"].append(f"{k}")
+                    top_words["scores"].append(f"{v}")
+            except Exception as e:
+                print(f"error occured: {e}")
+                pass
 
-@app.route("/", methods=['GET', 'POST'])
-def main_page():
-    if request.method == 'POST':
-        top_words = {"words":[], "scores":[]}
-        try:
-            results = lsh_main.search(request.get_json()['query'])
-            print(results)
-            for k, v in results.items():
-                top_words["words"].append(f"{k}")
-                top_words["scores"].append(f"{v}")
-        except Exception as e:
-            print(f"error occured: {e}")
-            pass
+            return json.dumps(top_words)
+        else:
+            return render_template('main.html')
 
-        return json.dumps(top_words)
-    else:
-        return render_template('main.html')
+    return app
 
 if __name__ == '__main__':
     from argparse import ArgumentParser
     parser = ArgumentParser()
-    parser.add_argument('--config', default='config_server.json', required=True)
-    args = parser.parse_args()
-    config = args.config
-    app, lsh_main = create_app(config)
+    parser.add_argument('--config', default='config_server.json', required=True) 
+    app = main(parser.parse_args().config)
     app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
 
 
